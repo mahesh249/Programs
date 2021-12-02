5457937d855819cd1e92261a6f145e2e05c1b95c
Mahesh Thorat <mahithorat249@gmail.com>
2021-12-02 08:00:35 +0530

    Added a.cpp file

---
+++ a.cpp
@@ -0,0 +1,19 @@
+#include<bits/stdc++.h>
+#define ll long long
+#define M 1000000007
+using namespace std;
+int main()
+{
+   ios::sync_with_stdio(0);
+   cin.tie(0);
+   srand(chrono::high_resolution_clock::now().time_since_epoch().count());
+   int t;
+   cin>>t;
+   while(t--){
+  
+    
+    
+   }
+   
+  return 0;
+}
